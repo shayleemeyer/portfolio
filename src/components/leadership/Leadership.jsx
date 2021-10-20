35564f9eb097c02e74c29d97cf2ac6d7cf747b5d
Shaylee Meyer <shayleeomeyer@gmail.com>
2021-10-20 10:51:10 -0500

    portfolio, work, leadership css update

--- src/components/leadership/Leadership.jsx
+++ src/components/leadership/Leadership.jsx
@@ -1,14 +1,13 @@
 
-/* import "./leadership.scss"; */
+import "./leadership.scss"; 
 
 import React, { Component } from "react";
-import Slider from "react-slick";
 
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+
+
 import { useState } from 'react';
 
-/* export default function Leadership() {
+ export default function Leadership() {
     const [currentSlide, setCurrentSlide] = useState(0);
     const data= [
         {
@@ -92,57 +91,6 @@ import { useState } from 'react';
         </div>
         
         
-    )} */
+    )} 
 
-    const data= [
-        {
-            id: "1",
-            title: "President, Alpha Xi Delta",
-            pointOne: "●	Voted 2019 President of the Year amongst 10 other Greek organizations",
-            pointTwo: "● Created tools using logical formulas to completely restructure organization of chapter finances.",
-            pointThree:"●	Responsible for all chapter related legalities and activities.  ",
-            img: "assets/alphaxicrest.png"
-        },
-        {
-            id: "2",
-            title: "Vice President, National Panhellenic Conference",
-            pointOne: "	●	Elected supervisor of Junior Executive Board",
-            pointTwo: "●	Coordination of events that saw the attendance of over 300 people. ",
-           
-            img: "assets/panhel.png"
-        },
-        {
-            id: "3",
-            title: "Vice President of Member Development, Alpha Xi Delta",
-            pointOne: "● Elected supervisor of Member Development council and all related activities ",
-            pointTwo: "●	Only elected Emerging Leader to attend leadership conference in Maryland.",
-            pointThree:"●	Event coordination fostering the growth of the chapter and individual members.",
-            img: "assets/alphaxi.jpg"
-        }
-     ];
-
-    export default class SimpleSlider extends Component {
-        render() {
-          const settings = {
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
-          };
-          return (
-            <div>
-              <h2> Single Item</h2>
-              <Slider {...settings}>
-               {data.map((data)=>{
-                   return(
-                       <div>
-                          <h1>7</h1> 
-                       </div>
-                   )
-               })}
-              </Slider>
-            </div>
-          );
-        }
-      }
+ 
