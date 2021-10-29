e69f8975898b473e76dfc85c61067891e9167a0c
Shaylee Meyer <shayleeomeyer@gmail.com>
2021-10-29 11:47:47 -0500

    all done with green scheme

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -7,8 +7,8 @@ export default function Testimonials() {
       name: "Ashly Pinarkyil",
       title: "Pricing Analyst at Medidata Solutions",
       img:
-        "assets/ashly.png",
-      icon: "assets/ashly.png",
+        "assets.nosync/ashlyp.png",
+      icon: "assets.nosync/ashlyp.png",
       desc:
         "Shaylee is a very hardworking individual. Working aside her during my summer internship at Federal Mogul Motorparts was an incredible experience. She is very motivated and organized individual that brings innovation and out of the box ideas to modern day problems. I have never worked with someone with such vast experiences. Shaylee is continually learning and strengthening her skills. It has been a pleasure working with her and being her colleague.",
         link: "https://www.linkedin.com/in/ashlypinarkyil/",
@@ -19,8 +19,8 @@ export default function Testimonials() {
       name: "Shane Mann",
       title: "Alternative Investments Accountant",
       img:
-        "assets/shane.jpg",
-      icon: "assets/twitter.png",
+        "assets.nosync/shane.jpg",
+      icon: "assets.nosync/twitter.png",
       desc:
         "Shaylee Meyer thrives in both individual and team environments. I was grateful to work with her in co-dependent departments at DRiV Inc. She possesses extreme attention to detail and invaluable communication skills. Though engineering was a foreign subject to me, Shaylee was able to communicate the impact and direction of projects she worked on, which is a transferrable and useful skill to any business. Shaylee has helped me develop as a professional in countless ways. I specifically recall her taking the time to listen and provide constructive feedback on a presentation I later gave to upper management at DRiV. She will be a leader in any role she pursues.",
         link: "https://www.linkedin.com/in/shane-mann61/",
@@ -31,8 +31,8 @@ export default function Testimonials() {
       name: "Tombe Mogga",
       title: "Corporate PR Representative at UPS",
       img:
-        "assets/tombe.png",
-      icon: "assets/tombe.png",
+        "assets.nosync/tombe.png",
+      icon: "assets.nosync/tombe.png",
       desc:
         "In life, there are few people an Individual meets that boasts all the greatness being a human expresses. Shaylee is the exact expression of this ideology; Greatness. As Archimedes said: Give me a lever long enough, and a place to stand, and I will move the earth. Shaylee will move the earth with whatever she creates.",
       link: "https://www.linkedin.com/in/tombe-mogga-837129b5/",
--- src/data.js
+++ src/data.js
@@ -1,100 +1,74 @@
 
-export const featuredPersonal = [
-    {
-      id: 1,
-      title: "Shaylee meyer",
-      img:
-        "assets/car.png",
-    },
-    {
-      id: 2,
-      title: "shay shay",
-      img:
-        "assets/car.png",
-    },
-    {
-      id: 3,
-      title: "shayls",
-      img:
-        "assets/car.png",
-    },
-    {
-      id: 4,
-      title: "Relax Mobile App",
-      img:
-        "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 5,
-      title: "Hero Web Design",
-      img:
-        "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 6,
-      title: "Banking App",
-      img:
-        "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
-    },
-  ];
-  
   export const rhondaPortfolio = [
     {
       id: 1,
       title: "Rhonda",
-      img: "assets/rhonda.jpg",
+      img: "assets.nosync/rhonda.jpg",
     },
     {
       id: 2,
       title: "Cool Bean",
-      img: "assets/coolbean.png",
+      img: "assets.nosync/coolbean.png",
     },
     {
       id: 3,
       title: "Cute Bean",
-      img: "assets/cutebean.jpg",
+      img: "assets.nosync/cutebean.jpg",
     },
     {
       id: 4,
       title: "Bean",
-      img: "assets/bean.jpg",
+      img: "assets.nosync/bean.jpg",
     },
     {
       id: 5,
       title: "Goofy Bean",
-      img: "assets/goofybean.png",
+      img: "assets.nosync/goofybean.PNG",
     },
     {
       id: 6,
       title: "Sleepy Bean",
-      img: "assets/sleepybean.png",
+      img: "assets.nosync/sleepybean.png",
     },
   ];
   
   export const philosophyPortfolio = [
     {
       id: 1,
-      title: "Seneca",
+      title: "Marcus Aurelius",
       img:
-        "assets/onangerwoods.png",
+        "assets.nosync/action.jpg",
     },
     {
       id: 2,
-      title: "Marcus Aurelius",
+      title: "Seneca",
       img:
-        "assets/impediment.png",
+        "assets.nosync/onanger.jpg",
     },
     {
       id: 3,
       title: "James A. Michener",
       img:
-        "assets/themaster.png",
+        "assets.nosync/themaster.jpg",
     },
+    
     {
       id: 4,
       title: "Epictetus",
       img:
-        "assets/tolearn.jpg",
+        "assets.nosync/tolearn.jpg",
+    },
+    {
+      id: 5,
+      title: "Marcus Aurelius",
+      img:
+        "assets.nosync/harmedbyfear.jpg",
+    },
+    {
+      id: 6,
+      title: "Marcus Aurelius",
+      img:
+        "assets.nosync/closerto.jpg",
     },
     
   ];
@@ -104,84 +78,84 @@ export const featuredPersonal = [
       id: 1,
       title: "The Obstacle is the Way",
       img:
-        "assets/obstacle.png",
+        "assets.nosync/obstacle.png",
       link: "https://www.amazon.com/Obstacle-Way-Timeless-Turning-Triumph/dp/1591846358/ref=sxts_rp_s1_0?cv_ct_cx=the+obstacle+is+the+way&dchild=1&keywords=the+obstacle+is+the+way&pd_rd_i=1591846358&pd_rd_r=8ffd9557-6ac6-4f1f-9d49-964bd7ec2333&pd_rd_w=nzmaE&pd_rd_wg=XadKX&pf_rd_p=60f680d5-2ef4-438b-a860-68ff8dd70684&pf_rd_r=3EAWWM0G9CR2RCFGFWW9&psc=1&qid=1633527962&sr=1-1-2a7ba1aa-1291-4447-a5e4-5bbe384024ef"
     },
     {
       id: 2,
       title: "Breaking the Habit of Being Yourself",
       img:
-        "assets/breakinghabit.jpg",
+        "assets.nosync/breakinghabit.jpg",
       link: "https://www.amazon.com/Breaking-Habit-Being-Yourself-Create/dp/1401938094/ref=sr_1_1?crid=324T2LK5BCBI7&dchild=1&keywords=breaking+the+habit+of+being+yourself&qid=1633528001&s=books&sprefix=breaking+the+habit+of+b%2Cstripbooks%2C164&sr=1-1"
     },
     {
       id: 3,
       title: "Emotional Intelligence: Authentic Leadership",
       img:
-        "assets/authentic.jpg",
+        "assets.nosync/authentic.jpg",
       link: "https://www.amazon.com/Authentic-Leadership-HBR-Emotional-Intelligence/dp/1633693910/ref=sr_1_3?crid=2QNJWZHX1YGLQ&dchild=1&keywords=emotional+intelligence+authentic+leadership&qid=1633528038&sprefix=emotional+intelligence+authentic+%2Cstripbooks%2C169&sr=8-3"
     },
     {
       id: 4,
       title: "Permission to Screw Up",
       img:
-        "assets/permission.jpg",
+        "assets.nosync/permission.jpg",
       link:"https://www.amazon.com/Permission-Screw-Up-Learned-Everything/dp/1591848296/ref=sr_1_1?dchild=1&keywords=permission+to+screw+up&qid=1633528073&sr=8-1"
     },
     {
       id: 5,
       title: "Seat of the Soul",
       img:
-        "assets/seatofsoul.jpg",
+        "assets.nosync/seatofsoul.jpg",
         link: "https://www.amazon.com/Seat-Soul-Anniversary-Study-Guide/dp/147675540X/ref=sr_1_1?dchild=1&keywords=seat+of+the+soul&qid=1633528092&sr=8-1"
     },
     {
       id: 6,
       title: "Stillness is the Key",
       img:
-        "assets/stillness.jpg",
+        "assets.nosync/stillness.jpg",
       link: "https://www.amazon.com/Stillness-Key-Ryan-Holiday/dp/0525538585/ref=sxts_rp_s1_0?crid=3FBZSTEQWH1IG&cv_ct_cx=stillness+is+the+key&dchild=1&keywords=stillness+is+the+key&pd_rd_i=0525538585&pd_rd_r=3d685a00-7fe6-4ee4-84ad-38d4c83fa8e0&pd_rd_w=m0KbA&pd_rd_wg=FsU4R&pf_rd_p=60f680d5-2ef4-438b-a860-68ff8dd70684&pf_rd_r=XPRBGG4ZQHDF48CDBFBT&psc=1&qid=1633528113&sprefix=stillness+is+th%2Caps%2C174&sr=1-1-2a7ba1aa-1291-4447-a5e4-5bbe384024ef"
     },
     {
         id: 7,
         title: "Ego is the Enemy",
         img:
-          "assets/ego.jpg",
+          "assets.nosync/ego.jpg",
         link: "https://www.amazon.com/Ego-Enemy-Ryan-Holiday/dp/1591847818/ref=sr_1_1?crid=5OOUSC7KRYYQ&dchild=1&keywords=ego+is+the+enemy&qid=1633528143&s=books&sprefix=ego+is+the+e%2Cstripbooks%2C173&sr=1-1"
       },
       {
         id: 8,
         title: "Warlight",
         img:
-          "assets/warlight.jpg",
+          "assets.nosync/warlight.jpg",
         link: "https://www.amazon.com/Warlight-Vintage-International-Michael-Ondaatje/dp/0525562966/ref=sr_1_1?dchild=1&keywords=warlight&qid=1633528165&s=books&sr=1-1"
       },
       {
         id: 9,
         title: "Atomic Habits",
         img:
-          "assets/atomic.jpg",
+          "assets.nosync/atomic.jpg",
         link: "https://www.amazon.com/Atomic-Habits-Proven-Build-Break/dp/0735211299/ref=sr_1_1?crid=1FZFTJWUZNZ6O&dchild=1&keywords=atomic+habits&qid=1633528189&s=books&sprefix=atomic%2Cstripbooks%2C188&sr=1-1"
       },
       {
         id: 10,
         title: "Meditations",
         img:
-          "assets/meditations.jpg",
+          "assets.nosync/meditations.jpg",
         link: "https://www.amazon.com/Meditations-150th-Anniversary-Collection-Philosophy/dp/B097C299F4/ref=sr_1_4?dchild=1&keywords=meditations&qid=1633528229&s=books&sr=1-4"
       },
       {
         id: 11,
         title: "The Daily Stoic",
         img:
-          "assets/dailystoic.jpg",
+          "assets.nosync/dailystoic.jpg",
         link: "https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance/dp/0735211736/ref=sr_1_1?crid=1LWM066BK4133&dchild=1&keywords=the+daily+stoic&qid=1633528289&s=books&sprefix=the+daily%2Cstripbooks%2C180&sr=1-1"
       },
       {
         id: 12,
         title: "Courage is Calling",
         img:
-          "assets/courage.jpg",
+          "assets.nosync/courage.jpg",
         link: "https://www.amazon.com/Courage-Calling-Fortune-Favors-Brave/dp/0593191676/ref=sr_1_2?crid=Z9ZTM9P9P01A&dchild=1&keywords=courage+is+calling+ryan+holiday&qid=1633528307&s=books&sprefix=courage+is+call%2Cstripbooks%2C228&sr=1-2"
       },
   ];
@@ -191,35 +165,35 @@ export const featuredPersonal = [
       id: 1,
       title: "Huberman Lab",
       img:
-        "assets/huberman.png",
+        "assets.nosync/huberman.png",
         link: "https://hubermanlab.com",
     },
     {
       id: 2,
       title: "Impact Theory",
       img:
-        "assets/bilyue.png",
+        "assets.nosync/bilyue.png",
         link: "https://impacttheory.com",
     },
     {
       id: 3,
       title: "Stuff To Blow Your Mind",
       img:
-        "assets/stuffto.png",
+        "assets.nosync/stuffto.png",
         link: "https://www.iheart.com/podcast/stuff-to-blow-your-mind-21123915/",
     },
     {
       id: 4,
       title: "Lex Fridman Podcast",
       img:
-        "assets/lex.png",
+        "assets.nosync/lex.png",
         link: "https://lexfridman.com/podcast/",
     },
     {
       id: 5,
       title: "The Ed Mylett Show",
       img:
-        "assets/mylett.png",
+        "assets.nosync/mylett.png",
         link: "https://www.edmylett.com/podcasts/",
     },
     {
@@ -235,35 +209,35 @@ export const featuredPersonal = [
       id:1,
       title: "React",
       img:
-          "assets/react.png",
+          "assets.nosync/react.png",
       link: "https://github.com/shayleemeyer/nucampReact"
     },
     {
       id:2,
       title: "React Native",
       img:
-          "assets/reactNative.png",
+          "assets.nosync/reactNative.png",
       link: "https://github.com/shayleemeyer/ReactNative"
     },
     {
       id:3,
       title: "Bootstrap",
       img:
-          "assets/bootstrap.png",
+          "assets.nosync/bootstrap.png",
       link: "https://github.com/shayleemeyer/nucampsiteBootstrap"
     },
     {
       id:4,
       title: "React",
       img:
-          "assets/center.png",
+          "assets.nosync/center.png",
       link: "https://github.com/shayleemeyer/centerutilization1"
     },
     {
       id:5,
       title: "React App",
       img:
-          "assets/courage.jpg",
+          "assets.nosync/courage.jpg",
       link: "link"
     }
   ]
