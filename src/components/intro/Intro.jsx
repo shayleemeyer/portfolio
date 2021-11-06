a0e62a34edbd847fc5bf6b805e3442fc4ce7657f
Shaylee Meyer <shayleeomeyer@gmail.com>
2021-11-06 13:07:21 -0500

    intro background image

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -19,16 +19,19 @@ export default function Intro() {
     return (
         <div className="introWrapper" id="intro" >
             <div className="introContent">
+           
                 <div className="wrapper">
+                <img src="/assets.nosync/branches.jpg" alt=""/>
                     <h2>Hi There, I'm</h2>
                     <h1>Shaylee Meyer</h1>
                     <h3>
                         The coolest <span ref={textRef}></span>
                     </h3>
                     <h4>(self-proclaimed^ )</h4>
+                    
                 </div>
                 <a href="#portfolio">
-                    <img src="assets.nosync/down.png" alt=''/>
+                    {/* <img src="assets.nosync/down.png" alt=''/> */}
                 </a>
             </div>
         </div>
